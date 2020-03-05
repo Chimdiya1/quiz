d3e853ede8f3dee7fec10bcd6375807901e17a7f
chimdia <fortunechimdiya@gmail.com>
2020-03-05 20:03:24 +0100

    Updates

---
+++ precache-manifest.fa0e1509a679c76bc8248b525e93f330.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87160250550d6b2e6bc040a82595d013",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "07a7a9d8142ec48ae9ef",
+    "url": "/quiz/static/css/main.70724368.chunk.css"
+  },
+  {
+    "revision": "626ec1537355548792f9",
+    "url": "/quiz/static/js/2.0490ee39.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/quiz/static/js/2.0490ee39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "07a7a9d8142ec48ae9ef",
+    "url": "/quiz/static/js/main.09ed49d1.chunk.js"
+  },
+  {
+    "revision": "8345eba1007debd37bf1",
+    "url": "/quiz/static/js/runtime-main.62537abc.js"
+  }
+]);
